bfb2f0e37059ff9f95ea560c759da45f11376dc6
Nazmul <nazmulislamn95@gmail.com>
2022-05-09 19:35:47 +0600

    JS Class

---
+++ style.js
@@ -0,0 +1,29 @@
+
+
+// class Car{
+//     constructor(name, year){
+//         this.name = name;
+//         this.year = year;
+//     }
+// }
+
+// const result = new Car("Toyota", 2022);
+// console.log(result);
+
+
+
+// let car = {type:"Fiat", model:"500", color:"white"};
+// car = "Fiat";      // Changes object to string
+// console.log(car);
+
+
+
+// const person = [];
+// person[0] = "John";
+// person[1] = "Doe";
+// person[2] = 46;
+// console.log(person.length);      // person.length will return 0
+// console.log(person[0]);
+
+
+
